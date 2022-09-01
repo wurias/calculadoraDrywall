4e977c810a60c3c1452f955181d5d9bc0754f798
wurias <w.sairu@disroot.org>
2022-09-01 19:49:38 -0300

    versão beta

--- calculaDrywall.js
+++ calculaDrywall.js
@@ -1,48 +1,62 @@
+//VARIAVEIS 
 var largura = document.querySelector('#largura');
 var altura = document.querySelector('#altura');
+var resposta = document.querySelector('#resposta');
+var quadradoTotal, cantoTotal, f530Total, stTotal;
 
 
-function escreva (texto){
-    document.write(texto);
+//FUNCAO CALCULA METRO-QUADRADO
+function metroQuadrado (larg, alt){
+    var larg = parseFloat(largura.value);
+    var alt = parseFloat(altura.value);   
+
+    var metroQuadrado = larg * alt;
+    quadradoTotal = "Total: " + metroQuadrado.toFixed(2) + "m² \n";
 }
-
-function calculadora (larg, alt) {
-
+//FUNCAO CALCULA CANTONEIRA
+function cantoneira (larg, alt){
     var larg = parseFloat(largura.value);
     var alt = parseFloat(altura.value);
 
-    //METRO QUADRADO
-    var metroQuadrado = larg * alt;    
-    escreva ('Largura: [' + larg.toFixed(2) + '] Comprimento: [' + alt.toFixed(2) + ']')
-    escreva ('<br><br>Total: ' + metroQuadrado.toFixed(2) + 'm²<br>');
+    var calculando = 2 * (larg + alt) / 3;
+    var cantoneiraTotal = Math.ceil(calculando);
+    cantoTotal = "Cantoneira: " + cantoneiraTotal + " und. \n";    
+}
+//FUNCAO CALCULA PERFIL F530
+function perfilF530 (larg, alt){
+    var larg = parseFloat(largura.value);
+    var alt = parseFloat(altura.value);    
 
-    //QUANTIDADE CANTONEIRA
-    var cantoneira = 2 * (larg + alt) / 3;
-    escreva ('<br>Cantoneira: ' + Math.ceil(cantoneira));
-
-    //PERFIL F530
     if (larg >= alt){
-
         var perfilF530 = larg / 0.60;
         var restoPerfil = alt - 3;
         var parcialPerfil = (perfilF530 * restoPerfil) /3;
-        var totalPerfil = parcialPerfil + perfilF530;
-        escreva ('<br>Perfil-F530: ' + Math.ceil(totalPerfil));
+        f530Total = "Perfil F530: " + Math.ceil(parcialPerfil + perfilF530) + " und. \n";
+        
 
-        } else {
-                
+    } else {
+
         var perfilF530 = alt / 0.60;
         var restoPerfil = larg - 3;
         var parcialPerfil = (perfilF530 * restoPerfil) / 3;
-        var totalPerfil = parcialPerfil + perfilF530;
-        escreva ('<br>Perfil-F530: ' + Math.ceil(totalPerfil));
-                
-        }
+        f530Total = "Perfil F530: " + Math.ceil(parcialPerfil + perfilF530) + " und. \n";        
+    }
+}
+//FUNCAO CALCULA PLACA ST TAMANDO PADRAO 180 X 120 ARREDONDANDO PARA MAIS
+function placaST (larg, alt){
+    var larg = parseFloat(largura.value);
+    var alt = parseFloat(altura.value);
 
-    //PLACAS, COM TAMANHO PADRAO 180 X 120, ARREDONDADO PARA MAIS
+    var metroQuadrado = (larg * alt);
     var placaST = metroQuadrado + (5 * metroQuadrado) / 100;
-    var placaTotal = placaST / 2.16;
-    escreva ('<br>PlacaST: ' + Math.ceil(placaTotal));
+    stTotal = "PlacaST: " + Math.ceil(placaST / 2.16) + " und. \n";
 
 }
 
+function listaGeral(){
+    metroQuadrado();
+    cantoneira();
+    perfilF530();
+    placaST();
+    resposta.value = quadradoTotal + cantoTotal + f530Total + stTotal;
+}
